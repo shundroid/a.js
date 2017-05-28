9b01a36c53f1bf71fc4b57d1b2b362186b19718f
shundroid <shundroidk@gmail.com>
2017-05-28 10:31:18 +0900

    wrote tests of <App />

--- test/components/AppTest.js
+++ test/components/AppTest.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from 'components/App';
+import Palette from 'containers/Palette';
+import Canvas from 'components/Canvas';
 
 describe('<App />', function () {
+  const colorTest = 'red';
+  const widthTest = 20;
+  const paletteTest = {
+    color: colorTest,
+    width: widthTest
+  };
 
   beforeEach(function () {
-    this.component = shallow(<App />);
+    this.component = shallow(<App palette={paletteTest} />);
   });
 
   describe('when rendering the component', function () {
-
     it('should have a className of "index"', function () {
       expect(this.component.hasClass('index')).to.equal(true);
+    });
+    it('should have a palette', function () {
+      expect(this.component.contains(<Palette />)).to.equal(true);
+    });
+    it('should have a canvas', function () {
+      expect(this.component.contains(
+        <Canvas color={paletteTest.color} width={paletteTest.width} />
+      )).to.equal(true);
     });
   });
 });
