38383c250bebf6e5554d16d0222fad4452ca444c
shundroid <shundroidk@gmail.com>
2017-05-28 13:06:17 +0900

    wrote tests for reducers/palette

--- test/reducers/paletteTest.js
+++ test/reducers/paletteTest.js
@@ -1,12 +1,26 @@
-var reducer = require('../../src/reducers/palette');
+import reducer from 'reducers/palette';
+import { CHANGE_COLOR, CHANGE_WIDTH } from 'actions/const';
+import config from 'config';
 
 describe('palette', () => {
-
-  it('should not change the passed state', (done) => {
-
-    const state = Object.freeze({});
-    reducer(state, {type: 'INVALID'});
-
-    done();
+  it('should return the initial state', () => {
+    expect(reducer(void 0, { })).to.deep.equal({
+      color: config.defaultColor,
+      width: config.defaultWidth
+    });
+  });
+  it('should handle CHANGE_COLOR', () => {
+    const color = '#010101';
+    expect(reducer(void 0, {
+      type: CHANGE_COLOR,
+      color
+    })).to.have.property('color', color);
+  });
+  it('should handle CHANGE_WIDTH', () => {
+    const width = 40;
+    expect(reducer(void 0, {
+      type: CHANGE_WIDTH,
+      width
+    })).to.have.property('width', width);
   });
 });
