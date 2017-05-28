b649bbd67f60cdeaba871259e4d0e3ebde82ea22
shundroid <shundroidk@gmail.com>
2017-05-28 12:33:28 +0900

    wrote tests for <Palette />

--- test/components/PaletteTest.js
+++ test/components/PaletteTest.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Palette from 'components/Palette.js';
+import colors from 'constants/colors';
 
 describe('<Palette />', function () {
 
   let component;
+  const testActions = {
+    changeWidth() {},
+    changeColor() {}
+  };
+  const testPalette = {
+    color: 'red',
+    width: 30
+  };
   beforeEach(function () {
-    component = shallow(<Palette />);
+    component = shallow(<Palette actions={testActions} palette={testPalette} />);
   });
 
   describe('when rendering the component', function () {
-
-    it('should have a className of "palette-component"', function () {
-      expect(component.hasClass('palette-component')).to.equal(true);
+    it('should make colors of pens', function () {
+      expect(component.find('Pen')).to.have.length(colors.length);
+    });
+    it('should have a <LineWidth />', function () {
+      expect(component.find('LineWidth')).to.have.length(1);
     });
   });
 });
