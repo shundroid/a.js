bdc30d16c87494921ee4bac482b35a14d26b9e26
shundroid <shundroidk@gmail.com>
2017-05-28 12:20:50 +0900

    wrote tests for <LineWidth />

--- test/components/LineWidthTest.js
+++ test/components/LineWidthTest.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
 import LineWidth from 'components/LineWidth.js';
 
 describe('<LineWidth />', function () {
 
   let component;
+  let testWidth;
+  let testOnChangeWidth;
   beforeEach(function () {
-    component = shallow(<LineWidth />);
+    testWidth = 10;
+    testOnChangeWidth = () => {};
+    component = shallow(<LineWidth width={testWidth} onChangeWidth={testOnChangeWidth} />);
   });
 
   describe('when rendering the component', function () {
+    it('should have an input', function () {
+      expect(component.find('input')).to.have.length(1);
+    });
+    it('should set default value by width', function () {
+      const width = 30;
+      component.setProps({ width });
+      expect(component.find('input').prop('defaultValue')).to.equal(width);
+    });
+  });
 
-    it('should have a className of "linewidth-component"', function () {
-      expect(component.hasClass('linewidth-component')).to.equal(true);
+  describe('mouseup', function () {
+    it('should call onChangeWidth with the value', function () {
+      const onChangeWidth = sinon.spy();
+      const value = '40';
+      component.setProps({ onChangeWidth });
+      component.find('input').simulate('mouseup', {
+        target: { value }
+      });
+      expect(onChangeWidth.withArgs(parseInt(value)).calledOnce).to.equal(true);
     });
   });
 });
