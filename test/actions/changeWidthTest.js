7c63485fd46bacf26d44b034724ba48f2f8c16f1
shundroid <shundroidk@gmail.com>
2017-05-30 16:32:18 +0900

    changeWidthTest width -> lineWidth

--- test/actions/changeWidthTest.js
+++ test/actions/changeWidthTest.js
@@ -3,10 +3,10 @@ import { CHANGE_WIDTH } from 'actions/const';
 
 describe('changeWidth', () => {
   it('should create an action to change the width', () => {
-    const width = 40;
-    expect(action(width)).to.deep.equal({
+    const lineWidth = 40;
+    expect(action(lineWidth)).to.deep.equal({
       type: CHANGE_WIDTH,
-      width
+      width: lineWidth
     });
   });
 });
