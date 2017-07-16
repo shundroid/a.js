bbd3201c0585834982fb8f3e63252f7fdfdcbc0d
shundroid <shundroidk@gmail.com>
2017-07-16 16:16:57 +0900

    supported monitoring lines and update canvas

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,7 @@ class AppComponent extends React.Component {
         <Canvas
           color={this.props.palette.color}
           width={this.props.palette.width}
+          lines={this.props.canvas.lines}
           onPenUp={this.props.actions.addLine} />
         <PenPreview lineWidth={this.props.palette.width} color={this.props.palette.color} />
       </div>
