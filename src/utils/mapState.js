a6d5168196714836b97360641e761edff87afd6d
shundroid <shundroidk@gmail.com>
2017-08-07 15:26:53 +0900

    implemented validateState

--- src/utils/mapState.js
+++ src/utils/mapState.js
@@ -3,6 +3,9 @@
 // { 'palette.color': PropTypes.string.isRequired }
 export class States {
   constructor(states) {
+    const errors = validateStates(states);
+    if (errors) throw new Error(errors);
+
     this.states = states;
   }
   toPropTypes() {
@@ -22,6 +25,15 @@ export class States {
       return states;
     };
   }
+}
+export function validateStates(states) {
+  const errors = [];
+  for (const stateName in states) {
+    if (stateName.indexOf('.') === -1) {
+      errors.push(`Invalid state name: ${stateName}`);
+    }
+  }
+  return errors;
 }
 export default function mapState(states) {
   return new States(states);
