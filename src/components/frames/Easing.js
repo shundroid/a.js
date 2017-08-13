b39152c5d78559bd1b57b1c590e5eda23b771982
shundroid <shundroidk@gmail.com>
2017-08-13 12:56:39 +0900

    support show/hide easing panel

--- src/components/frames/Easing.js
+++ src/components/frames/Easing.js
@@ -4,13 +4,29 @@ import cssmodules from 'react-css-modules';
 import styles from '@components/frames/easing.cssmodule.styl';
 
 class Easing extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isShow: false };
+  }
+  toggle = () => {
+    this.setState({ isShow: !this.state.isShow });
+  }
   change = value => {
     console.log(value);
+  }
+  getStyle() {
+    return {
+      display: this.state.isShow ? 'block' : 'none'
+    };
   }
   render() {
     return (
       <div styleName="easing">
-        <BezierEasingEditor onChange={this.change} />
+        <button styleName="easing-button" onClick={this.toggle}>E</button>
+        <BezierEasingEditor
+          onFinish={this.change}
+          styleName="easing-editor"
+          style={this.getStyle()} />
       </div>
     );
   }
