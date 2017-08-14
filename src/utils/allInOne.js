713b3a0fb17ba22dfb18fb15fd0d7bae3802448e
shundroid <shundroidk@gmail.com>
2017-08-14 21:57:34 +0900

    fixed allInOne eslint

--- src/utils/allInOne.js
+++ src/utils/allInOne.js
@@ -3,10 +3,13 @@ import cssmodules from 'react-css-modules';
 import mapState from '@utils/mapState';
 import mapDispatch from '@utils/mapDispatch';
 
-export default function allInOne(_component, style = null, _props = {}, _actions = [], styleOptions = {}) {
+export default function allInOne(
+  _component, style = null, _props = {}, _actions = [], styleOptions = {}) {
   const props = mapState(_props);
   const actions = mapDispatch(..._actions);
-  _component.propTypes = { ..._component.propTypes, ...props.toPropTypes(), ...actions.toPropTypes() };
+  _component.propTypes = {
+    ..._component.propTypes, ...props.toPropTypes(), ...actions.toPropTypes()
+  };
   const component = style ? cssmodules(_component, style, styleOptions) : _component;
   return connect(props.toConnect(), actions.toConnect())(component);
 }
